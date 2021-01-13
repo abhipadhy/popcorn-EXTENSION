fa276278a408f14ddfe50b7eab6c8aaf0e0b184c
abhipadhy <abhishekpadhyakr@gmail.com>
2021-01-13 20:56:17 +0530

    initial

---
+++ index.js
@@ -0,0 +1,31 @@
+function trending(){
+
+    fetch('https://api.themoviedb.org/3/trending/all/week?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb')
+    .then(response => response.json())
+    .then(function(data){
+        for(i=0;i<10;i++){
+            document.getElementById(`c${i}`).style.backgroundImage= `linear-gradient(180deg, rgba(10, 10, 10, 0) 13.99%, #0A0A0A 100%),url('https://image.tmdb.org/t/p/original${data.results[i].poster_path}')`;
+            if(data.results[i].media_type === 'movie'){
+                
+                
+            }
+            else{
+                
+            }
+            
+        }
+        
+    })
+};
+
+trending();
+
+// $('.card').click(function () {
+//     var value =$(this).index('.card');
+//     fetch('https://api.themoviedb.org/3/trending/all/week?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb')
+//     .then(response => response.json())
+//     .then(function(data){
+//         document.querySelector('body').style.backgroundImage= `linear-gradient(180deg, rgba(10, 10, 10, 0) 63.99%, #0A0A0A 100%),url('https://image.tmdb.org/t/p/original${data.results[value].backdrop_path}')`;
+//         document.querySelector('.card').style.display="none"
+//     })
+// });
