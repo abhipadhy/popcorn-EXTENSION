7949de34e859f66863773c11e5b58ae0267e7f63
abhipadhy <abhishekpadhyakr@gmail.com>
2021-01-14 00:24:43 +0530

    version 1

---
+++ config.js
@@ -0,0 +1,4 @@
+
+export function key(){
+    return '15d2ea6d0dc1d476efbca3eba2b9bbfb';
+}
--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-function trending(){
+function key (){
+    fetch('https://popcornapi2.herokuapp.com/getkey')
+    .then(response => response.json())
+    .then(function (data){
+        trending(data);
+    })
+};
 
-    fetch('https://api.themoviedb.org/3/trending/all/week?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb')
+function trending(data){
+
+    fetch('https://api.themoviedb.org/3/trending/all/week?api_key='+data.key)
     .then(response => response.json())
     .then(function(data){
         var innerdata=``;
@@ -9,13 +17,13 @@ function trending(){
             if(data.results[i].media_type === 'movie'){
                 let a=`  <div class="card">
         
-                <div class="pbox" id="c${i}"></div>
+                <div class="pbox" id="c${i}"><a href="" style="text-decoration:none;" target="_blank"><p>watch trailer</p></a></div>
         
                 <ul id="l6">
-                    Name:<li id="name">${data.results[i].original_title}</li><br>
-                    release:<li id="year">${data.results[i].release_date}</li><br>
-                    media type:<li id="type">${data.results[i].media_type}</li><br>
-                    imdb:<li id="imdb">${data.results[i].vote_average}</li><br>
+                    Name:<li id="name">${data.results[i].original_title}</li>
+                    release:<li id="year">${data.results[i].release_date}</li>
+                    media type:<li id="type">${data.results[i].media_type}</li>
+                    imdb:<li id="imdb">${data.results[i].vote_average}</li>
                 </ul>
             </div>` 
             innerdata+=a;
@@ -25,13 +33,13 @@ function trending(){
             else{
                 let a=`  <div class="card">
         
-                <div class="pbox" id="c${i}"></div>
+                <div class="pbox" id="c${i}"><a href="" style="text-decoration:none;" target="_blank"><p>watch trailer</p></a></div>
         
                 <ul id="l6">
-                    Name:<li id="name">${data.results[i].original_name}</li><br>
-                    release:<li id="year">${data.results[i].first_air_date}</li><br>
-                    media type:<li id="type">${data.results[i].media_type}</li><br>
-                    imdb:<li id="imdb">${data.results[i].vote_average}</li><br>
+                    Name:<li id="name">${data.results[i].original_name}</li>
+                    release:<li id="year">${data.results[i].first_air_date}</li>
+                    media type:<li id="type">${data.results[i].media_type}</li>
+                    imdb:<li id="imdb">${data.results[i].vote_average}</li>
                 </ul>
             </div>`
             innerdata+=a;
@@ -47,14 +55,14 @@ function trending(){
     })
 };
 
-trending();
+key();
 
-// $('.card').click(function () {
-//     var value =$(this).index('.card');
+// $('.content').click(function () {
+//     var value =$(this).index('.content');
 //     fetch('https://api.themoviedb.org/3/trending/all/week?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb')
 //     .then(response => response.json())
 //     .then(function(data){
 //         document.querySelector('body').style.backgroundImage= `linear-gradient(180deg, rgba(10, 10, 10, 0) 63.99%, #0A0A0A 100%),url('https://image.tmdb.org/t/p/original${data.results[value].backdrop_path}')`;
-//         document.querySelector('.card').style.display="none"
+        
 //     })
 // });
